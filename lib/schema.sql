b9d209525ef1f30e2fa7655ce47dd70fd271ecfa
Meg Galvez <meg@oakmaine.com>
2019-06-24 21:59:07 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE series(id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_genre_id INTEGER);
+CREATE TABLE series(id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
 
 CREATE TABLE sub_genre(id INTEGER PRIMARY KEY, name TEXT);
 
