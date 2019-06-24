a0ba1df276e9ea635bdef95c700a38285f1859eb
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:10:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,5 +8,7 @@ INSERT INTO authors(name) VALUES ("JRR Tolkien");
 INSERT INTO authors(name) VALUES ("Brandon Sanderson");
 
 INSERT INTO books(title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
+INSERT INTO books(title, year, series_id) VALUES ("The Two Towers", 1954, 1);
+INSERT INTO books(title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
 INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2001, 2)
 
