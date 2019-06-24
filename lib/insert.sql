9b3c8f780b9488dadeb4ffbe69fd09c4b4f4af49
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:09:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,4 +7,6 @@ INSERT INTO subgenres(name) VALUES ("fantasy");
 INSERT INTO authors(name) VALUES ("JRR Tolkien");
 INSERT INTO authors(name) VALUES ("Brandon Sanderson");
 
-INSERT INTO books(title, year, series_id) VALUES ()
+INSERT INTO books(title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
+INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2001, 2)
+
