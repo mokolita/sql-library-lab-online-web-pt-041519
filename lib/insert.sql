4a4b08ef0c8a131f8abab99f58f0100c7f6d5570
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:17:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,3 +19,4 @@ INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Gand
 INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Tom Bombadil", "I'm a bear", "bear_man", 1, 1);
 INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("The Mouth of Sauron", "I have no eyes", "beast", 1, 1);
 INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Kahladin", "WHINE", "Human", 2, 2);
+INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Kahladin", "WHINE", "Human", 2, 2);
