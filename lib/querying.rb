652a3f6cfb71a809fec1a06ee0a97f79ea761065
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:29:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.titles, books.years 
+  FROM books
+  WHERE series.id = 1 
+  ORDER BY books.years"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
