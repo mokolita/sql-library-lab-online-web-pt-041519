04db427482fbe5127c15f351d71c9df132aeef5e
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:51:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(species) AS species_count
   FROM characters
   GROUP BY species
-  ORDER BY species_count
+  ORDER BY species_count DESC
   LIMIT 1"
 end
 
