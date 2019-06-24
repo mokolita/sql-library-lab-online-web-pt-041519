0bdaab16416a0fa09ba4ae46c52391c81960e147
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:25:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,12 +23,12 @@ INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Shal
 INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Jasnau", "The desolation", "Princess", 2, 2);
 INSERT INTO characters(name, motto, species, author_id, series_id) VALUES ("Sin", "float float", "Spren", 2, 2);
 
-INSERT INTO character_books(character_id, book_id) VALUES (1, 1)
-INSERT INTO character_books(character_id, book_id) VALUES (1, 2)
-INSERT INTO character_books(character_id, book_id) VALUES (1, 3)
-INSERT INTO character_books(character_id, book_id) VALUES (2, 1)
-INSERT INTO character_books(character_id, book_id) VALUES (2, 2)
-INSERT INTO character_books(character_id, book_id) VALUES (2, 3)
+INSERT INTO character_books(character_id, book_id) VALUES (1, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 2);
+INSERT INTO character_books(character_id, book_id) VALUES (1, 3);
+INSERT INTO character_books(character_id, book_id) VALUES (2, 1);
+INSERT INTO character_books(character_id, book_id) VALUES (2, 2);
+INSERT INTO character_books(character_id, book_id) VALUES (2, 3);
 INSERT INTO character_books(character_id, book_id) VALUES (3, 1)
 INSERT INTO character_books(character_id, book_id) VALUES (4, 3)
 INSERT INTO character_books(character_id, book_id) VALUES (5, 1)
