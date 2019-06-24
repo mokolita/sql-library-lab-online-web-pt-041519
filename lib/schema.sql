fd5d3966198e1092b3685c83ad245257c3595c5c
Meg Galvez <meg@oakmaine.com>
2019-06-24 21:54:05 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -3,3 +3,9 @@ CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_
 CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT);
 
 CREATE TABLE authors (id PRIMARY KEY, name TEXT)
+
+CREATE TABLE books (id PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+
+CREATE TABLE characters (id PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
+
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id INTEGER, book_id INTEGER);
