31ce50de1e9ca9708b54d7550285847664a8e0c8
Meg Galvez <meg@oakmaine.com>
2019-06-24 23:19:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,5 +41,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id) AS total_books
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
-  JOIN books ON books.id = character_books.book_id"
+  GROUP BY "
 end
