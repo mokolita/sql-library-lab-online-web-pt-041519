938ab7d9bd755d036d5812a58e2b9743e68b3c26
Meg Galvez <meg@oakmaine.com>
2019-06-24 23:05:31 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,9 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
-  JOIN "
+  JOIN characters ON characters.series_id = series.id
+  GROUP BY series DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
