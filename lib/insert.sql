139f128d62ff23dfe4647c81b3b43e7aa50d9415
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:07:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,10 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 2);
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Way of Kings", 2, 2);
 
-INSERT INTO subgenres(name) VALUES ("fantasy");
 INSERT INTO subgenres(name) VALUES ("SCIFI");
+INSERT INTO subgenres(name) VALUES ("fantasy");
 
-INSERT INTO authors(na)
+INSERT INTO authors(name) VALUES ("JRR Tolkien");
+INSERT INTO authors(name) VALUES ("Brandon Sanderson");
+
+INSERT INTO books(title, year, series_id) VALUES ()
