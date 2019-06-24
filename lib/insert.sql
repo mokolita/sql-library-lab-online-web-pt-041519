ff6931a315990dcfcaee6f4f9b56624befa1e04e
Meg Galvez <meg@oakmaine.com>
2019-06-24 22:11:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,6 +10,6 @@ INSERT INTO authors(name) VALUES ("Brandon Sanderson");
 INSERT INTO books(title, year, series_id) VALUES ("The Fellowship of the Ring", 1954, 1);
 INSERT INTO books(title, year, series_id) VALUES ("The Two Towers", 1954, 1);
 INSERT INTO books(title, year, series_id) VALUES ("Return of the King", 1955, 1);
-INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2001, 2)
+INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2010, 2)
 INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2001, 2)
 INSERT INTO books(title, year, series_id) VALUES ("The Way of Kings", 2001, 2)
