100ece3aeb4c722a3ac2ed011d864ecb8e7be58f
Meg Galvez <meg@oakmaine.com>
2019-06-24 23:20:47 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,5 +42,5 @@ def select_character_names_and_number_of_books_they_are_in
   FROM character_books
   JOIN characters ON character_books.character_id = characters.id
   GROUP BY characters.name
-  ORDER BY total_books"
+  ORDER BY total_books DESC"
 end
